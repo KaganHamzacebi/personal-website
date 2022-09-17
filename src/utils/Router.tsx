14147d34ca44e6dc31c3f28cf7a160eeba591a9c
KaganHamzacebi <kagan.hcebi@gmail.com>
2022-09-18 02:16:29 +0300

    scss structure built

---
+++ src/utils/Router.tsx
@@ -0,0 +1,21 @@
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
+import {Home} from "../views/Home/Home";
+
+function Router() {
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Home />,
+        },
+    ]);
+
+    return (
+        <RouterProvider router={router} />
+    )
+}
+
+export default Router;
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+
+    return (
+        <div>
+            123
+        </div>
+    )
+}
