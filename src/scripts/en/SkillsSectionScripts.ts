7b22ac2fdc97df1706b702b9ed8b8d5e29f79b89
KaganHamzacebi <kagan.hcebi@gmail.com>
2022-10-20 01:35:58 +0300

    react skill script updated

--- src/scripts/en/SkillsSectionScripts.ts
+++ src/scripts/en/SkillsSectionScripts.ts
@@ -2,7 +2,8 @@ import {ISkillsSectionScripts} from "../../features/languageController/LanguageC
 
 const SkillsSectionScripts = {
     "react_h1": "React.JS",
-    "react_desc": "React bla bla",
+    "react_desc": "React is my favorite frontend framework and Im still learning about it. It is very deep and enjoyable. " +
+        "I mostly used hooks but also have a limited knowledge to old class components.",
     "node_h1": "Node.JS",
     "node_desc": "React bla bla",
     "html_h1": "HTML",
