a05e1da8c1683561c8f6c7f2ef4037e47b9c7cc3
KaganHamzacebi <kagan.hcebi@gmail.com>
2022-10-31 02:30:01 +0300

    database uri fix

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -3,7 +3,7 @@ import {connect} from 'mongoose';
 export default async function connectMongoDB() {
     const NODE_ENV = process.env.NODE_ENV;
     const connectionString = NODE_ENV === 'development' ?
-        'mongodb://cl9usb6v6001gbgmt2vik5cnv:0EZxaq0AgH6UgGfKwRYtyeLa@158.101.214.51:9000/?readPreference=primary&ssl=false'
+        'mongodb://localhost:27017/personal_website'
         :
         process.env.DB_URI;
     await connect(connectionString);
