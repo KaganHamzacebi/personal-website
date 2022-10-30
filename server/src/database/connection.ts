caa188e484b5de3e9c9b39907c7435ecdc2cba90
KaganHamzacebi <kagan.hcebi@gmail.com>
2022-10-30 23:50:12 +0300

    connection string update

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,6 +1,6 @@
 import {connect} from 'mongoose';
 
 export default async function connectMongoDB() {
-    const connectionString = "mongodb://cl9usb6v6001gbgmt2vik5cnv:0EZxaq0AgH6UgGfKwRYtyeLa@nope:9000/?readPreference=primary&ssl=false";
+    const connectionString = "mongodb://cl9usb6v6001gbgmt2vik5cnv:0EZxaq0AgH6UgGfKwRYtyeLa@158.101.214.51:9000/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
     await connect(connectionString);
 }
